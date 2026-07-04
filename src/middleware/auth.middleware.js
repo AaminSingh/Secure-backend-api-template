eeac164bf68a830587ac34df473c8f922d3bb195
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-07-04 13:22:12 +0530

    updating the auth middleware file

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,9 @@
 import { User } from "../models/user.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 import {ApiError} from "../utils/api-error.js";
 import {asyncHandler} from "../utils/async-handler.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 export const verifyJWT = asyncHandler(async(req, res,next)=>{
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")      // here we are using optional chaining to check if the accessToken is present in the cookies or in the header and we are replacing the "Bearer " from the token if it is present in the header
@@ -22,3 +24,29 @@ export const verifyJWT = asyncHandler(async(req, res,next)=>{
         throw new ApiError(401,"Invalide access token!");
       }
 })
+
+export const validateProjectPermission = (roles = []) =>{
+  asyncHandler(async(req,res,next)=>{
+    const {projectId} = req.params
+    if(!projectId){
+      throw new ApiError(400,"ProjectId is required")
+    }
+   const project =  await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id)
+    })
+
+    if(!project){
+      throw new ApiError(400,"Project not found or user is not a member.")
+    }
+    const givenRole = project?.role      //role extracted from the project itself
+    req.user.role = givenRole
+
+    if(!roles.includes(givenRole)){
+      throw new ApiError (403,
+        "you do not have permission to perform this action")
+    }
+     next()
+
+  })
+}
