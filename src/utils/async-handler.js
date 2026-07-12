9f2f8b50c3e581396dd2098d7bb1bfa562d3334e
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-07-12 14:33:02 +0530

    updating the async file

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,7 +1,8 @@
 const asyncHandler = (requestHandler)=>{
     return (req,res,next) => {
         Promise
-        .resolve(requestHandler(req,res,next))
+        .resolve(requestHandler(req,res,next))       //It returns a new middleware function.
+                                                     //Promise.resolve() simply makes sure we're working with a Promise.
         .catch((err) => next(err))
     }
 };
