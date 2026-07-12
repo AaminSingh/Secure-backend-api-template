0cc1c10015b9bcdc4cbc69f21e50b46d1a817f30
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-07-12 16:16:19 +0530

    updating the file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,8 @@ export const UserRolesEnum = {
     MEMBER:"member"
 }
 
-export const AvailableUserRoles = Object.values(UserRolesEnum);
-export const AvailableUserRole = AvailableUserRoles;
+export const AvailableUserRoles = Object.values(UserRolesEnum);      //this will give us an array of all the values in the UserRolesEnum object. because we want to use this array in our validation schema to check if the role provided by the user is valid or not.
+export const AvailableUserRole = AvailableUserRoles;                   //this is just an alias for AvailableUserRoles, we can use either of them in our validation schema.
 
 export const TaskStatusEnum = {
     TODO:"todo",
