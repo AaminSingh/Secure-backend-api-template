2bf2b1fc4927a5001e5ff1fdfa444d173a0642fa
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-07-20 21:25:46 +0530

    updating the file

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -6,7 +6,6 @@ const storage = multer.diskStorage({
     },
     filename:function(req,file,cb){
         cb(null,`${Date.now()}-${file.originalname}`)
-    
     }
 })
 export const upload = multer({
